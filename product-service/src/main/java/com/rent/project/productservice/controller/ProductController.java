3b00c381331e4c58c73980a966c1b645e757f560
Aditi <aditi@bbd.co.za>
2022-04-20 23:16:50 +0530

    adding product service changes

--- product-service/src/main/java/com/rent/project/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/rent/project/productservice/controller/ProductController.java
@@ -1,12 +1,13 @@
 package com.rent.project.productservice.controller;
 
 import com.rent.project.productservice.models.Product;
-import com.rent.project.productservice.repository.ProductRepo;
 import com.rent.project.productservice.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -15,16 +16,19 @@ public class ProductController {
 
     @Autowired
     ProductService ps;
-    @PostMapping("/add")
-    public ResponseEntity<Product> addproduct(@RequestBody Product Product)
-    {
-        return ps.addProduct(Product);
-    }
 
+
+    @PostMapping("/add")
+    public ResponseEntity<Product> addproduct(Product product,@ModelAttribute("file") MultipartFile file) throws IOException {
+        return ps.addProduct(product,file);
+    }
+//    @PostMapping("/upload")
+//    public String upload(@RequestParam("file") MultipartFile file) throws IOException {
+//        return ps.uploadFile(file);
+//    }
     @PutMapping("/edit/{Id}")
-    public ResponseEntity<Product> editproduct(@PathVariable Long Id, @RequestBody Product Product)
-    {
-        return ps.editProduct(Id,Product);
+    public ResponseEntity<Product> editproduct(@PathVariable Long Id, Product Product,@ModelAttribute("file") MultipartFile file) throws IOException {
+        return ps.editProduct(Id,Product,file);
     }
 
     @DeleteMapping("/delete/{Id}")
